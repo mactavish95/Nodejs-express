78f5daaa9f92951325343afaa7f3af371457ec73
CpPrice <ghostware@ubuntu.ubuntu-domain>
2020-05-21 22:42:11 +0700

    add new files

--- 1-start/index.js
+++ 1-start/index.js
@@ -1,15 +1,40 @@
+console.log(process.env);
+
 var express = require('express');
 var app = express();
+var cookieParser = require('cookie-parser')
 
+var userRoute = require('./routes/user.route');
+var authRoute = require('./routes/auth.route'); 
+
+var authMiddleware = require('./middlewares/auth.middleware');
+
+var port = 3000;
+
+app.engine('pug', require('pug').__express);
+app.set('view engine', 'pug');
+app.set('views', './views');
+
+app.use(express.json()) // for parsing application/json
+app.use(express.urlencoded({ extended: true })) // for parsing application/x-www-form-urlencoded
+
+app.use(cookieParser('adsfsafsdffd21'));
+
+app.use(express.static('public'));
+
+
+// Route
 app.get('/', function(request, response) {
-	response.send('<h1>Hello world I am coder</h1>');
+	response.render('index', {
+		name: 'AAA'
+	});
 });
 
 
-app.get('/users', function(request, response) {
-	response.send('Users list');
-});
 
-app.listen(3000, function() {
-	console.log('Server listening on port 3000')
+app.use('/users', authMiddleware.requireAuth, userRoute);
+app.use('/auth' ,authRoute);
+
+app.listen(port, function() {
+	console.log('Server listening on port' + port);
 });
